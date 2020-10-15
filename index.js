35f19fdb431be8a313a72e662c931087f9741590
Annisa Purbandari <a.purbandari@gmail.com>
2020-10-15 14:27:41 +1100

    added generatefile function

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const fs = require('fs');
+const axios = require('axios');
 const path = require('path');
 const util = require('util');
 const inquirer = require('inquirer');
 
+
+const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 const input = [
     {
         type: "input",
-        name: "GH",
-        message: "Please type your GitHub username?",
+        name: "GitHub",
+        message: "Please type your GitHub username",
         default: 'annisapf'
     },
     {
         type: "input",
-        name: "fName",
-        message: "Please provide the name for a .md file?",
+        name: "fileName",
+        message: "Please provide the name for a .md file",
         default: 'README'
     },
     {
         type: "input",
         name: "email",
-        message: "Please type your email?",
+        message: "Please type your email",
         default: "a.purbandari@gmail.com",
+        validate: validateEmail
     },
     {
         type: "input",
         name: "URL",
-        message: "the URL to your project?",
+        message: "Please type the URL to your project",
         default: 'GoodReadMe'
     },
     {
         type: "input",
         name: "title",
-        message: "Please type your project's name?",
+        message: "Please type your project's name",
         default: "GoodREADME.md generator"
     },
     {
@@ -43,13 +51,13 @@ const input = [
     {
         type: "list",
         name: "license",
-        message: "What kind of license should your project have?",
+        message: "Please choose a licence below",
         choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     },
     {
         type: "list",
         name: "color",
-        message: "What's your favorite color?",
+        message: "Please choose your favourite colour",
         choices: ["red", "green", "purple", "black", "magenta"]
     },
     {
@@ -67,24 +75,121 @@ const input = [
     {
         type: "input",
         name: "usage",
-        message: "What does the user need to know about using the repo?",
+        message: "What does the user need to know about using the repository?",
         default: "It is an open project and everyone can contribute - please send and email requesting to be added as a contributor!"
     },
     {
         type: "input",
         name: "contributing",
-        message: "What does the user need to know about contributing to the repo?",
+        message: "What does the user need to know about contributing to the repository?",
         default: 'this is an open source app - anyone can contribute'
     }
 ]
 
-function createReadme(name, data) {
-    return fs.writeFileSync(path.join(process.cwd(), name), data);
+function writeReadMeFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            throw err;
+        }
+    })
 }
 
-function showQuestion() {
-    inquirer.prompt(input).then(responses =>
-        createReadme(`${responses.fName}.md`))
+function init() {
+    inquirer.prompt(input).then(function (data) {
+        console.log(data);
+        let url = "https://api.github.com/users/" + data.Username + "/events/public"
+
+        axios.get(url).then(function (response) {
+
+            let email = response.data;
+            data["email"] = email
+
+            writeReadMeFile("test.md", generateReadMeFile(data))
+        })
+
+    });
 }
 
-showQuestion()
+init();
+
+function getGitHubLink(GitHub, link) {
+    return `http://github.com/${GitHub}/${link}`;
+}
+
+function getBadge(licence, GitHub, title, color, link) {
+    if (licence !== 'None') {
+        return `[![GitHub licence](https://img.shields.io/badge/)-${color}.svg)](${getGitHubLink(GitHub, title, link)})`
+    } else {
+        return ``
+    }
+}
+
+function getLicense(license) {
+    if (license !== 'None') {
+        return `
+    ## License
+
+    License is ${license} standard license.`
+    } else {
+        return ``
+    }
+}
+
+
+function generateReadMeFile(data) {
+    return `
+    # ${data.title}
+    ${getBadge(data.license, data.GitHub, data.title, data.color, data.URL)}
+    
+    ## Description
+    
+    ${data.description}
+    
+    ## Table of Contents 
+    
+    * [Installation](#installation)
+    
+    * [Usage](#usage)
+    
+    * [License](#license)
+    
+    * [Contributing](#contributing)
+    
+    * [Tests](#tests)
+    
+    * [Questions](#questions)
+    
+    ## Installation
+    
+    To install necessary dependencies, run the following command:
+    
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+    
+    ## Usage
+    
+    ${data.usage}
+    
+    ${getLicense(data.license)}
+        
+    ## Contributing
+    
+    ${data.contributing}
+    
+    ## Tests
+    
+    To run tests, run the following command:
+    
+    \`\`\`
+    ${data.test}
+    \`\`\`
+    
+    ## Questions
+    
+    If you have any questions about the repo, open an issue or contact [${data.GitHub}](https://github.com/annisapf/) directly at ${data.email}.`;
+}
+
+
+
+
